b373968ff632b3ef548e2df276d92d43724bf082
dave665@gmail.com <dave665@gmail.com>
2022-09-27 22:36:35 +0000

    fixes

--- models/sources/streamkap/admin_user.sql
+++ models/sources/streamkap/admin_user.sql
@@ -3,4 +3,4 @@
     unique_by='user_id'
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='admin_user')}}
+{{streamkap_incremental_on_source_to_current(source_name='admin_user', id_field='user_id')}}
