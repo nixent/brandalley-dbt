cfecf92aec13f5494855570358bdec4e21009904
djayatillake <dave665@gmail.com>
2022-10-05 11:38:54 +0100

    eav_attribute_value unique key fix

--- models/sources/streamkap_current/stg__eav_attribute_option_value.sql
+++ models/sources/streamkap_current/stg__eav_attribute_option_value.sql
@@ -1,6 +1,6 @@
 {{config(
     materialized='incremental',
-    unique_key='value_id'
+    unique_key=['option_id', 'store_id']
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='eav_attribute_option_value', id_field='value_id')}}
+{{streamkap_incremental_on_source_to_current(source_name='eav_attribute_option_value', id_field='option_id, store_id')}}
