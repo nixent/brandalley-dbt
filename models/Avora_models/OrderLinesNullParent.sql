9ebf59d1710657efaf396b4da9110df8df5b7065
dave665@gmail.com <dave665@gmail.com>
2022-10-13 10:52:42 +0000

    formatting

--- models/Avora_models/OrderLinesNullParent.sql
+++ models/Avora_models/OrderLinesNullParent.sql
@@ -1,59 +1,70 @@
-select  
-case 
-when cpev.value like ('Home>%') then "Homeware"
-when cpev.value like ('Home & Garden>%') then "Homeware"
-when cpev.value like ('Gifts>%') then "Festival"
-when cpev.value like ('Kids>%') then "Kidswear"
-when cpev.value like ('Christmas>%') then "Festival"
-when cpev.value like ('Lingerie>%') then "Lingerie & Swimwear"
-when cpev.value like ('Outlet>%') then "Outlet"
-when cpev.value like ('Men>Tops%') then "RTW"
-when cpev.value like ('Men>Trousers>%') then "RTW"
-when cpev.value like ('Men>Underwear>%') then "Lingerie & Swimwear"
-when cpev.value like ('Men>Accessories>%') then "Accessories"
-when cpev.value like ('Men>Clothing>%') then "RTW"
-when cpev.value like ('Men>Fleeces%') then "RTW"
-when cpev.value like ('Men>Jeans>%') then "RTW"
-when cpev.value like ('Men>Knitwear>%') then "RTW"
-when cpev.value like ('Men>Onesies>%') then "RTW"
-when cpev.value like ('Men>Outerwear>%') then "RTW"
-when cpev.value like ('Men>Shirts>%') then "RTW"
-when cpev.value like ('Men>Footwear>%') then "Footwear"
-when cpev.value like ('Men>Shoes>%') then "Footwear"
-when cpev.value like ('Men>Shorts>%') then "RTW"
-when cpev.value like ('Men>Sportswear%') then "RTW"
-when cpev.value like ('Men>Suits>%') then "RTW"
-when cpev.value like ('Men>Swimwear>%') then "Lingerie & Swimwear"
-when cpev.value like ('Men>Nightwear>%') then "Lingerie & Swimwear"
-when cpev.value like ('Sports%') then "Active"
-when cpev.value like ('Women>Accessories%') then "Accessories"
-when cpev.value like ('Women>Blouses%') then "RTW"
-when cpev.value like ('Women>Clothing>%') then "RTW"
-when cpev.value like ('Women>Dresses>%') then "RTW"
-when cpev.value like ('Women>Fleeces%') then "RTW"
-when cpev.value like ('Women>Footwear%') then "Footwear"
-when cpev.value like ('Women>Handbags>%') then "Accessories"
-when cpev.value like ('Women>Jeans>%') then "RTW"
-when cpev.value like ('Women>Knitwear>%') then "RTW"
-when cpev.value like ('Women>Lingerie>%') then "Lingerie & Swimwear"
-when cpev.value like ('Women>Onesies>%') then "RTW"
-when cpev.value like ('Women>Outerwear>%') then "RTW"
-when cpev.value like ('Women>Playsuits%') then "RTW"
-when cpev.value like ('Women>Shoes%') then "Footwear"
-when cpev.value like ('Women>Shorts>%') then "RTW"
-when cpev.value like ('Women>Skirts>%') then "RTW"
-when cpev.value like ('Women>Sportswear%') then "Active"
-when cpev.value like ('Women>Tops%') then "RTW"
-when cpev.value like ('Women>Trousers%') then "RTW"
-when cpev.value like ('Women>Maternity%') then "RTW"
-when cpev.value like ('Women>Jeans%') then "RTW"
-when cpev.value like ('Women>Outerwear%') then "RTW"
-when cpev.value like ('Beauty>%') then "Beauty"
-else "Others" end as Category,
-sfo.status,
-sfo.customer_id,
-sfo.increment_id
-from {{ ref('stg__sales_flat_order') }} sfo
-left join {{ ref('stg__customer_entity') }} ce on ce.entity_id = sfo.customer_id
-left join {{ ref('stg__sales_flat_order_item') }} sfoi on sfo.entity_id = sfoi.order_id and sfoi.parent_item_id is null
-left join {{ ref('stg__catalog_product_entity_varchar') }} cpev ON cpev.entity_id = sfoi.product_id and cpev.attribute_id = 205
+SELECT
+    CASE
+        WHEN cpev.value LIKE ('Home>%') THEN "Homeware"
+        WHEN cpev.value LIKE ('Home & Garden>%') THEN "Homeware"
+        WHEN cpev.value LIKE ('Gifts>%') THEN "Festival"
+        WHEN cpev.value LIKE ('Kids>%') THEN "Kidswear"
+        WHEN cpev.value LIKE ('Christmas>%') THEN "Festival"
+        WHEN cpev.value LIKE ('Lingerie>%') THEN "Lingerie & Swimwear"
+        WHEN cpev.value LIKE ('Outlet>%') THEN "Outlet"
+        WHEN cpev.value LIKE ('Men>Tops%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Trousers>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Underwear>%') THEN "Lingerie & Swimwear"
+        WHEN cpev.value LIKE ('Men>Accessories>%') THEN "Accessories"
+        WHEN cpev.value LIKE ('Men>Clothing>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Fleeces%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Jeans>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Knitwear>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Onesies>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Outerwear>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Shirts>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Footwear>%') THEN "Footwear"
+        WHEN cpev.value LIKE ('Men>Shoes>%') THEN "Footwear"
+        WHEN cpev.value LIKE ('Men>Shorts>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Sportswear%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Suits>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Men>Swimwear>%') THEN "Lingerie & Swimwear"
+        WHEN cpev.value LIKE ('Men>Nightwear>%') THEN "Lingerie & Swimwear"
+        WHEN cpev.value LIKE ('Sports%') THEN "Active"
+        WHEN cpev.value LIKE ('Women>Accessories%') THEN "Accessories"
+        WHEN cpev.value LIKE ('Women>Blouses%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Clothing>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Dresses>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Fleeces%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Footwear%') THEN "Footwear"
+        WHEN cpev.value LIKE ('Women>Handbags>%') THEN "Accessories"
+        WHEN cpev.value LIKE ('Women>Jeans>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Knitwear>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Lingerie>%') THEN "Lingerie & Swimwear"
+        WHEN cpev.value LIKE ('Women>Onesies>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Outerwear>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Playsuits%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Shoes%') THEN "Footwear"
+        WHEN cpev.value LIKE ('Women>Shorts>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Skirts>%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Sportswear%') THEN "Active"
+        WHEN cpev.value LIKE ('Women>Tops%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Trousers%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Maternity%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Jeans%') THEN "RTW"
+        WHEN cpev.value LIKE ('Women>Outerwear%') THEN "RTW"
+        WHEN cpev.value LIKE ('Beauty>%') THEN "Beauty"
+        ELSE "Others"
+    END AS category,
+    sfo.status,
+    sfo.customer_id,
+    sfo.increment_id
+FROM
+    {{ ref('stg__sales_flat_order') }}
+    sfo
+    LEFT JOIN {{ ref('stg__customer_entity') }}
+    ce
+    ON ce.entity_id = sfo.customer_id
+    LEFT JOIN {{ ref('stg__sales_flat_order_item') }}
+    sfoi
+    ON sfo.entity_id = sfoi.order_id
+    AND sfoi.parent_item_id IS NULL
+    LEFT JOIN {{ ref('stg__catalog_product_entity_varchar') }}
+    cpev
+    ON cpev.entity_id = sfoi.product_id
+    AND cpev.attribute_id = 205
