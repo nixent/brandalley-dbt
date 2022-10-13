952ddb0be24e4855e43f2d5722da888268b78cd6
djayatillake <dave665@gmail.com>
2022-10-13 15:28:08 +0100

    stg__catalog_category_product incremental fix

--- models/sources/streamkap_current/stg__catalog_category_product.sql
+++ models/sources/streamkap_current/stg__catalog_category_product.sql
@@ -1,8 +1,8 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = 'category_id, product_id'
+    unique_key = ['category_id', 'product_id']
 ) }}
 {{ streamkap_incremental_on_source_to_current(
     source_name = 'catalog_category_product',
-    id_field = 'category_id, product_id'
+    id_field = config.get('unique_key')
 ) }}
