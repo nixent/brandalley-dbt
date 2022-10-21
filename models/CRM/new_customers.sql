306280094e9c8737d6cd609215e5f0750d9ea907
djayatillake <dave665@gmail.com>
2022-10-21 00:19:00 -0500

    new_customers fix

--- models/CRM/new_customers.sql
+++ models/CRM/new_customers.sql
@@ -1,12 +1,30 @@
 SELECT
-    {{ dbt_utils.star(ref('stg__customer_entity')) }},
+    `e`.`entity_id`,
+    `e`.`entity_type_id`,
+    `e`.`attribute_set_id`,
+    `e`.`website_id`,
+    `e`.`email`,
+    `e`.`group_id`,
+    `e`.`increment_id`,
+    `e`.`store_id`,
+    `e`.`created_at`,
+    `e`.`updated_at`,
+    `e`.`is_active`,
+    `e`.`disable_auto_group_change`,
     `at_emarsys_sync_status`.`value` AS `emarsys_sync_status`,
     `at_emarsys_sync_banned`.`value` AS `emarsys_sync_banned`,
     `ns`.`subscriber_id`,
     `ns`.`subscriber_status`,
     `ns`.`subscriber_confirm_code`,
     `ns`.`subscriber_source`,
-    {{ dbt_utils.star(ref('stg__customer_entity_extra')) }}
+    `cee`.`line_id`,
+    `cee`.`customer_id`,
+    `cee`.`number_transactions`,
+    `cee`.`ltv`,
+    `cee`.`ltvr`,
+    `cee`.`first_purchase`,
+    `cee`.`last_purchase`,
+    `cee`.`first_visit_at`
 FROM
     {{ ref('stg__customer_entity') }} AS `e`
     LEFT JOIN {{ ref('stg__customer_entity_int') }} AS `at_emarsys_sync_status`
