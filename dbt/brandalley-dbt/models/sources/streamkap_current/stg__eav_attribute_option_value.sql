defbcadeb81fd1100da3de0db89bce296b471ce6
djayatillake <dave665@gmail.com>
2022-11-04 17:58:17 +0000

    switching offset field for eav to value_id

--- dbt/brandalley-dbt/models/sources/streamkap_current/stg__eav_attribute_option_value.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/stg__eav_attribute_option_value.sql
@@ -5,5 +5,6 @@
 ) }}
 {{ streamkap_incremental_on_source_to_current(
     source_name = 'eav_attribute_option_value',
-    id_field = config.get('unique_key')
+    id_field = config.get('unique_key'),
+    order_offset_field = 'value_id'
 ) }}
