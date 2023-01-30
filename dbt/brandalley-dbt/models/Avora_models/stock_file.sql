7653af831f8e7f4ff7500e527ccc7f6027a9593c
Guillaume Vachey <guillaume.vachey@avora.com>
2023-01-30 16:00:49 +0000

    Stock file fix

--- dbt/brandalley-dbt/models/Avora_models/stock_file.sql
+++ dbt/brandalley-dbt/models/Avora_models/stock_file.sql
@@ -34,160 +34,160 @@ SELECT
     CONCAT(au.firstname, ' ', au.lastname) AS buyer
 FROM
 		{{ ref(
-				'catalog_product_entity'
+				'stg__catalog_product_entity'
 		) }}
 		e
         INNER JOIN
 		{{ ref(
-				'cataloginventory_stock_item'
+				'stg__cataloginventory_stock_item'
 		) }}
 		stock ON stock.product_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_super_link'
+				'stg__catalog_product_super_link'
 		) }}
 		parent_relation ON parent_relation.product_id = e.entity_id
         INNER JOIN
 		{{ ref(
-				'catalog_product_entity'
+				'stg__catalog_product_entity'
 		) }}
 		parent_entity_relation ON parent_entity_relation.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_category_product'
+				'stg__catalog_category_product'
 		) }}
 		category ON category.product_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		image ON image.attribute_id = 85
         AND image.entity_id = e.entity_id
         INNER JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpvn ON cpvn.attribute_id = 71
         AND cpvn.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_int'
+				'stg__catalog_product_entity_int'
 		) }}
 		cpevsi ON cpevsi.attribute_id = 239
         AND cpevsi.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_supplier'
+				'stg__catalog_product_supplier'
 		) }}
 		cpevsiv ON cpevsi.value = cpevsiv.supplier_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_int'
+				'stg__catalog_product_entity_int'
 		) }}
 		cpeib ON cpeib.attribute_id = 178
         AND cpeib.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpevcoorigin ON cpevcoorigin.attribute_id = 117
         AND cpevcoorigin.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_decimal'
+				'stg__catalog_product_entity_decimal'
 		) }}
 		cpedcost ON cpedcost.attribute_id = 79
         AND cpedcost.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_parent_gender ON cpev_parent_gender.attribute_id = 180
         AND cpev_parent_gender.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_simple_gender ON cpev_simple_gender.attribute_id = 180
         AND cpev_simple_gender.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_simple_type ON cpev_simple_type.attribute_id = 179
         AND cpev_simple_type.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_parent_type ON cpev_parent_type.attribute_id = 179
         AND cpev_parent_type.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_int'
+				'stg__catalog_product_entity_int'
 		) }}
 		cpei_size ON cpei_size.attribute_id = 177
         AND cpei_size.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_int'
+				'stg__catalog_product_entity_int'
 		) }}
 		cpei_colour ON cpei_colour.attribute_id = 213
         AND cpei_colour.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_decimal'
+				'stg__catalog_product_entity_decimal'
 		) }}
 		cpedprice ON cpedprice.attribute_id = 75
         AND cpedprice.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_decimal'
+				'stg__catalog_product_entity_decimal'
 		) }}
 		cpedsprice ON cpedsprice.attribute_id = 76
         AND cpedsprice.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_decimal'
+				'stg__catalog_product_entity_decimal'
 		) }}
 		cpedoprice ON cpedoprice.attribute_id = 224
         AND cpedoprice.entity_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_outlet_category ON cpev_outlet_category.attribute_id = 205
         AND cpev_outlet_category.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_super_link'
+				'stg__catalog_product_super_link'
 		) }}
 		parent_relation_child ON parent_relation_child.parent_id = parent_relation.parent_id
         LEFT JOIN
 		{{ ref(
-				'cataloginventory_stock_item'
+				'stg__cataloginventory_stock_item'
 		) }}
 		stock_child ON stock_child.product_id = parent_relation_child.product_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_barcode ON cpev_barcode.attribute_id = 252
         AND cpev_barcode.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_entity_varchar'
+				'stg__catalog_product_entity_varchar'
 		) }}
 		cpev_nego ON cpev_nego.attribute_id = 204
         AND cpev_nego.entity_id = e.entity_id
         LEFT JOIN
 		{{ ref(
-				'catalog_product_negotiation'
+				'stg__catalog_product_negotiation'
 		) }}
 		cpn ON cpn.negotiation_id = cpev_nego.value
         LEFT JOIN
         {{ ref(
-                'admin_user'
+                'stg__admin_user'
         ) }}
         au ON cpn.buyer = au.user_id
 WHERE
