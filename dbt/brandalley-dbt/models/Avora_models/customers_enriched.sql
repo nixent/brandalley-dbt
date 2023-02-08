81cdabcd084e9a2c0f2aa5090dee58a4f3ffc8a2
joshua olufemi <Joshua.smart.olufemi@brandalley.co.uk>
2023-02-08 13:16:04 +0000

    1. changing ref function to point to Orders table in orders_enriched model. 2. removing the customers_enriched date filter and table from the orders_enriched model 3. renaming the order_ts column to order_at

--- dbt/brandalley-dbt/models/Avora_models/customers_enriched.sql
+++ dbt/brandalley-dbt/models/Avora_models/customers_enriched.sql
@@ -2,7 +2,7 @@ WITH
   customers AS (
   SELECT
     customer_id,
-    MIN(created_at) cohort_ts
+    MIN(created_at) first_purchase_cohort
   FROM {{ ref('OrderLines') }}
   GROUP BY 1 
   )
--- dbt/brandalley-dbt/models/Avora_models/orders_enriched.sql
+++ dbt/brandalley-dbt/models/Avora_models/orders_enriched.sql
@@ -1,21 +1,17 @@
 WITH customers_orders AS (
   SELECT
     o.customer_id,
-    o.order_id,
-    MIN(created_at) order_ts
-  FROM `datawarehouse-358408.analytics_magento.OrderLines` as o
-  JOIN {{ ref('customers_enriched') }} as c
-  ON
-    o.customer_id = c.customer_id
-    AND c.cohort_ts >= '2021-01-01'
+    o.magentoID AS order_id,
+    MIN(created_at) order_at 
+  FROM {{ ref('Orders') }} as o
   GROUP BY  1, 2 )
 
   , order_rank AS (
   SELECT
     customer_id,
     order_id,
-    order_ts,
-    RANK() OVER (PARTITION BY customer_id ORDER BY order_ts) order_sequence
+    order_at,
+    RANK() OVER (PARTITION BY customer_id ORDER BY order_at) order_sequence
   FROM customers_orders )
 
   SELECT * FROM order_rank 
