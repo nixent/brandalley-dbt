2bd0dc894252d6363f84d19193a398ddf2e5ab4c
Guillaume Vachey <guillaume.vachey@avora.com>
2023-02-09 15:36:25 +0000

    Wrong naming

--- dbt/brandalley-dbt/models/magento/sales/coupon_rules.sql
+++ dbt/brandalley-dbt/models/magento/sales/coupon_rules.sql
@@ -1,4 +1,4 @@
 select *, times_used >= usage_limit and (expiration_date is null or current_timestamp < expiration_date) as coupon_expired from 
 {{ ref(
-        'stg__couponrules_standard'
+        'stg__salesrule_coupon'
     ) }}
