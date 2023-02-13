2ed32a36adfbedb2f0821935ab15909761a4b8e6
Guillaume Vachey <guillaume.vachey@avora.com>
2023-02-13 16:02:15 +0000

    coupon_rules and gift cards updates, v1

--- dbt/brandalley-dbt/models/magento/sales/coupon_rules.sql
+++ dbt/brandalley-dbt/models/magento/sales/coupon_rules.sql
@@ -1,4 +1,15 @@
-select *, times_used >= usage_limit and (expiration_date is null or current_timestamp < expiration_date) as coupon_expired from 
+select sc.coupon_id,sc.rule_id,sc.code,sc.usage_limit,sc.usage_per_customer,sc.times_used,sc.expiration_date,sc.is_primary,sc.created_at as sales_rule_coupon_created_at,type, 
+sc.times_used >= sc.usage_limit and (sc.expiration_date is null or current_timestamp < expiration_date) as coupon_expired, 
+sfo.increment_id as order_id, sfo.status, sfo.base_discount_amount, sfo.base_discount_invoiced, sfo.discount_description,
+ia.id invent_autocoupon_id,ia.referral_coupon,ia.referee_coupon,ia.reward_to,ia.reward_from,ia.created_at as invent_autocoupon_created_at, ia.cartrule_id, ia.basis_for_issue,
+ia.comments_text,ia.recommendation,ia.referral_couponid,ia.referee_couponid,ia.reward_from_old,ia.issuer_id
+ from 
 {{ ref(
         'stg__salesrule_coupon'
-    ) }}
+    ) }} sc
+left outer join {{ ref(
+        'stg__sales_flat_order'
+    ) }} sfo on sc.code=sfo.coupon_code 
+left outer join {{ ref(
+        'stg__invent_autocoupon'
+    ) }} ia on sc.coupon_id=ia.referral_couponid and (ia.reward_from = sfo.customer_id or sfo.customer_id is null) 
---
+++ dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__ enterprise_giftcardaccount.sql
@@ -0,0 +1,10 @@
+{{config(
+    materialized='incremental',
+    unique_key='giftcardaccount_id',
+	cluster_by='giftcardaccount_id',
+)}}
+
+select giftcardaccount_id,code,status,date(date_created) date_created,date(date_expires) date_expires,website_id,balance,state,is_redeemable, 
+_streamkap_source_ts_ms, _streamkap_ts_ms, __deleted, _streamkap_offset, _streamkap_loaded_at_ts
+from (
+{{streamkap_incremental_on_source_to_current(source_name='giftcardaccount_id', id_field=config.get('unique_key'))}})
