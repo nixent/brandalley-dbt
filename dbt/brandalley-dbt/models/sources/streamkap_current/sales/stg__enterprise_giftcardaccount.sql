5a5560b4a1021f7170ab6e26901ae93565e86ba4
Guillaume Vachey <guillaume.vachey@avora.com>
2023-02-13 16:24:18 +0000

    Base definition incorrect for new tables

--- dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__enterprise_giftcardaccount.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__enterprise_giftcardaccount.sql
@@ -7,4 +7,4 @@
 select giftcardaccount_id,code,status,date(date_created) date_created,date(date_expires) date_expires,website_id,balance,state,is_redeemable, 
 _streamkap_source_ts_ms, _streamkap_ts_ms, __deleted, _streamkap_offset, _streamkap_loaded_at_ts
 from (
-{{streamkap_incremental_on_source_to_current(source_name='giftcardaccount_id', id_field=config.get('unique_key'))}})
+{{streamkap_incremental_on_source_to_current(source_name='enterprise_giftcardaccount', id_field=config.get('unique_key'))}})
--- dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__enterprise_giftcardaccount_history.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__enterprise_giftcardaccount_history.sql
@@ -1,9 +1,9 @@
 {{config(
     materialized='incremental',
-    unique_key='coupon_id',
-	cluster_by='coupon_id',
+    unique_key='history_id',
+	cluster_by='history_id',
 )}}
 
-select coupon_id, rule_id, code, usage_limit, usage_per_customer, times_used, timestamp(expiration_date) expiration_date, is_primary, timestamp(created_at) created_at,
+select history_id,giftcardaccount_id,timestamp(updated_at) as updated_at,action,balance_amount,balance_delta,additional_info,
 type, _streamkap_source_ts_ms, _streamkap_ts_ms, __deleted, _streamkap_offset, _streamkap_loaded_at_ts from (
-{{streamkap_incremental_on_source_to_current(source_name='salesrule_coupon', id_field=config.get('unique_key'))}})
+{{streamkap_incremental_on_source_to_current(source_name='enterprise_giftcardaccount_history', id_field=config.get('unique_key'))}})
