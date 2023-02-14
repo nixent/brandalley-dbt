adfb266256f179913b513c3a4ca2b2a7b9b78cfa
Guillaume Vachey <guillaume.vachey@avora.com>
2023-02-14 14:25:46 +0000

    Couple of missing things for coupon and gift cards

--- dbt/brandalley-dbt/models/magento/sales/gift_cards.sql
+++ dbt/brandalley-dbt/models/magento/sales/gift_cards.sql
@@ -1,6 +1,6 @@
 select 
 sfo.increment_id as order_id, sfo.status, sfo.base_gift_cards_amount, sfo.gift_cards_amount, sfo.base_gift_cards_invoiced, sfo.gift_cards_invoiced, sfo.base_gift_cards_refunded, 
-sfo.gift_cards_refunded, sfo.gw_id, sfo.gw_allow_gift_receipt, sfo.gw_add_card, sfo.gw_base_price, sfo.gw_price, sfo.gw_items_base_price, sfo.gw_items_price, sfo.gw_card_base_price, 
+sfo.gift_cards_refunded, timestamp(sfo.created_at) order_date, sfo.gw_id, sfo.gw_allow_gift_receipt, sfo.gw_add_card, sfo.gw_base_price, sfo.gw_price, sfo.gw_items_base_price, sfo.gw_items_price, sfo.gw_card_base_price, 
 sfo.gw_card_price, sfo.gw_base_tax_amount, sfo.gw_tax_amount, sfo.gw_items_base_tax_amount, sfo.gw_items_tax_amount, sfo.gw_card_base_tax_amount, sfo.gw_card_tax_amount, 
 sfo.gw_base_price_invoiced, sfo.gw_price_invoiced, sfo.gw_items_base_price_invoiced, sfo.gw_items_price_invoiced, sfo.gw_card_base_price_invoiced, sfo.gw_card_price_invoiced, 
 sfo.gw_base_tax_amount_invoiced, sfo.gw_tax_amount_invoiced, sfo.gw_items_base_tax_invoiced, sfo.gw_items_tax_invoiced, sfo.gw_card_base_tax_invoiced, sfo.gw_card_tax_invoiced, 
