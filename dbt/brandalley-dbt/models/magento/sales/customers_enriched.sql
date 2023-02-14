08a07f2ce5e1faafed4edbb3e1720d84217ec1de
Nicholas Lupu <njlupu@gmail.com>
2023-02-14 11:27:43 +0000

    trigger rebuild

--- dbt/brandalley-dbt/models/magento/sales/customers_enriched.sql
+++ dbt/brandalley-dbt/models/magento/sales/customers_enriched.sql
@@ -1,7 +1,7 @@
 with customers as (
   select
     customer_id,
-    min(created_at)   as first_purchase_at,
+    min(created_at)   as first_purchase_at, 
     count(magentoID)  as count_orders
   from {{ ref('Orders') }}
   group by 1 
