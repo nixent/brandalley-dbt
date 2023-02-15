b9fdd02d6342decbb6ac1b55f9362a96e16448e0
Nicholas Lupu <njlupu@gmail.com>
2023-02-15 17:49:22 +0000

    refund metrics for dedupe

--- dbt/brandalley-dbt/models/magento/sales/orders_enriched.sql
+++ dbt/brandalley-dbt/models/magento/sales/orders_enriched.sql
@@ -9,6 +9,18 @@ with order_line_agg as (
     sum(line_product_cost_exc_vat)       as order_product_costs_excl_tax
   from {{ ref('OrderLines') }}
   group by 1
+),
+
+order_refunds_agg as (
+  select
+    sfc.order_id,
+    count(sfc.entity_id)  as count_refunds,
+    count(sfci.entity_id) as count_item_refunds,
+    sum(row_total)        as total_refund_amount
+  from {{ ref('sales_flat_creditmemo') }} sfc
+  left join {{ ref('sales_flat_creditmemo_item') }} sfci
+    on sfci.parent_id = sfc.entity_id
+  group by 1
 )
 
 select
@@ -24,8 +36,13 @@ select
   o.days_since_signup,
   ola.order_revenue_excl_tax_after_vouchers,
   ola.order_product_costs_excl_tax,
-  ola.order_revenue_excl_tax_after_vouchers - ola.order_product_costs_excl_tax as order_margin
+  ola.order_revenue_excl_tax_after_vouchers - ola.order_product_costs_excl_tax as order_margin,
+  ora.count_refunds,
+  ora.count_item_refunds,
+  ora.total_refund_amount
 from {{ ref('orders_incremental') }} o
 left join order_line_agg ola 
   on o.magentoID = ola.order_id
+left join order_refunds_agg ora
+  on o.order_id = ora.order_id
 
