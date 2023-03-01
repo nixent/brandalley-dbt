0d21df653d4f79149a843e76b946652e31ec9b1c
Nicholas Lupu <njlupu@gmail.com>
2023-03-01 14:56:28 +0000

    fixes for resetting dev db

--- dbt/brandalley-dbt/macros/reset_dev_db.sql
+++ dbt/brandalley-dbt/macros/reset_dev_db.sql
@@ -5,18 +5,51 @@ Run using
 
 -#}
 
+{# Drop all schemas in dev to ensure full data replacement #}
+{% set schemas_in_dev %}
+    select 
+        catalog_name, 
+        schema_name
+    from datawarehouse-dev-371019.region-europe-west2.INFORMATION_SCHEMA.SCHEMATA
+{% endset %}
+
+{% set dev_schema_results = run_query(schemas_in_dev) %}
+
+{% for row in dev_schema_results %}
+    {% set drop_schema_sql = 'DROP SCHEMA IF EXISTS `datawarehouse-dev-371019`.' ~ row[1] ~ ' CASCADE;' %}
+    {{ log(drop_schema_sql, True) }}
+    {% do run_query(drop_schema_sql) %}
+{% endfor %}
+
+{# Recreate all prod schemas in dev db #}
+{% set schemas_in_prod %}
+    select 
+        catalog_name, 
+        schema_name
+    from datawarehouse-358408.region-europe-west2.INFORMATION_SCHEMA.SCHEMATA
+{% endset %}
+
+{% set prod_schema_results = run_query(schemas_in_prod) %}
+
+{% for row in prod_schema_results %}
+    {% set create_schema_sql = 'CREATE SCHEMA `datawarehouse-dev-371019`.' ~ row[1] ~ ' OPTIONS(location="europe-west2");' %}
+    {{ log(create_schema_sql, True) }}
+    {% do run_query(create_schema_sql) %}
+{% endfor %}
+
+{# Clone all tables in prod schemas #}
 {% set tables_in_prod %}
     select 
         table_catalog, 
         table_schema||'.'||table_name as table_name
     from region-europe-west2.INFORMATION_SCHEMA.TABLES 
-    where table_type = 'BASE TABLE' and table_schema in ('magento', 'emarsys', 'streamkap_current', 'streamkap')
+    where table_type = 'BASE TABLE'
 {% endset %}
 
-{% set results = run_query(tables_in_prod) %}
+{% set prod_tables_results = run_query(tables_in_prod) %}
 
-{% for row in results %}
-    {% set clone_table_sql = 'CREATE OR REPLACE TABLE datawarehouse-dev-371019.' ~ row[1] ~ ' CLONE datawarehouse-358408.' ~ row[1] ~ ';' %}
+{% for row in prod_tables_results %}
+    {% set clone_table_sql = 'CREATE OR REPLACE TABLE `datawarehouse-dev-371019`.' ~ row[1] ~ ' CLONE `datawarehouse-358408`.' ~ row[1] ~ ';' %}
     {{ log(clone_table_sql, True) }}
     {% do run_query(clone_table_sql) %}
   {% endfor %}
