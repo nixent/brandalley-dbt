c8234485b69c5effba052013545c5c130c9016aa
Nicholas Lupu <njlupu@gmail.com>
2023-03-06 18:04:42 +0000

    split products into separate model

--- dbt/brandalley-dbt/models/google_analytics/ga_daily_product_stats.sql
+++ dbt/brandalley-dbt/models/google_analytics/ga_daily_product_stats.sql
@@ -18,11 +18,16 @@ select
     trafficSource.source                                                                        as traffic_source,
     device.browser                                                                              as device_browser,
     device.operatingSystem                                                                      as device_os,
+    product.productSKU                                                                          as product_sku,
+    product.v2productName                                                                       as product_name,
+    product.productBrand                                                                        as product_brand,
+    sum(product.productQuantity)                                                                as product_quantity,
     count(distinct hits.transaction.transactionid)                                              as transactions,
-    coalesce(sum(hits.transaction.transactionRevenue)/1000000,0)                                as gmv,
+    coalesce(sum(product.productRevenue)/1000000,0)                                             as gmv,
     count(distinct fullVisitorId || visitId)                                                    as unique_visits
 from {{ source('76149814', 'ga_sessions_*') }},
-    unnest(hits) as hits
+    unnest(hits) as hits,
+    unnest(hits.product) as product
 where totals.visits = 1
     {% if is_incremental() %}
         and parse_date("%Y%m%d", date) > (select max(date) from {{this}})
