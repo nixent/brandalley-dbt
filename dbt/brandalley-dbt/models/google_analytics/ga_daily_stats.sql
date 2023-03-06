df94ff277715804a9900eb6cbca901b963ef208c
Nicholas Lupu <njlupu@gmail.com>
2023-03-06 12:51:27 +0000

    add traffic and device to GA stats

--- dbt/brandalley-dbt/models/google_analytics/ga_daily_stats.sql
+++ dbt/brandalley-dbt/models/google_analytics/ga_daily_stats.sql
@@ -5,6 +5,15 @@
 
 select
     parse_date("%Y%m%d", date)                                                                                                              as date,
+    channelGrouping                                                                                                                         as traffic_channel,
+    trafficSource.medium                                                                                                                    as traffic_medium,
+    trafficSource.campaign                                                                                                                  as traffic_campaign,
+    trafficSource.source                                                                                                                    as traffic_source,
+    device.browser                                                                                                                          as device_browser,
+    device.operatingSystem                                                                                                                  as device_os,
+    count(distinct hits.transaction.transactionid)                                                                                          as transactions,
+    coalesce(sum(hits.transaction.transactionrevenue)/1000000,0)                                                                            as revenue,
+    count(distinct concat(cast(fullvisitorid as string), cast(visitstarttime as string)))                                                   as unique_visits,
     count(distinct hits.transaction.transactionid)                                                                                          as transactions,
     sum(hits.transaction.transactionrevenue)/1000000                                                                                        as revenue,
     count(distinct concat(cast(fullvisitorid as string), cast(visitstarttime as string)))                                                   as unique_visits,
