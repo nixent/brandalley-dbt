0dd2ab4a4b870047758616d00eaffbad872b001a
Nicholas Lupu <njlupu@gmail.com>
2023-03-17 13:28:06 +0000

    change hourly sales today calcs

--- dbt/brandalley-dbt/models/magento/sales/orders/hourly_sales_today.sql
+++ dbt/brandalley-dbt/models/magento/sales/orders/hourly_sales_today.sql
@@ -21,18 +21,48 @@ with metrics_today_and_last_week as (
     group by 1,2,3,4,5,6,7,8
 ),
 
-margin_calcs as (
-    select
-        *,
-        round(total_revenue_exc_tax - line_product_cost_exc_vat_metric,2)                                                                              as margin_value,
-        if(extract(hour from created_at_hour) < 10, '0', '') || extract(hour from created_at_hour) || ':00'                                            as hour,
-        lag(total_revenue_exc_tax - line_product_cost_exc_vat_metric ) over (partition by extract(hour from created_at_hour) order by created_at_hour) as last_week_margin,
-        lag(qty_invoiced_metric) over (partition by extract(hour from created_at_hour) order by created_at_hour)                                       as last_week_qty_invoiced,
-        lag(total_revenue_exc_tax) over (partition by extract(hour from created_at_hour) order by created_at_hour)                                     as last_week_revenue
-    from metrics_today_and_last_week mt
-)
 
-select 
-    *
-from margin_calcs
+select
+    created_at_hour,
+    nego,
+    brand,
+    category_name,
+    department_type,
+    parent_sku,
+    product_type,
+    supplier_name,
+    qty_invoiced_metric,
+    total_revenue,
+    line_product_cost_exc_vat_metric,
+    total_revenue_exc_tax,
+    round(total_revenue_exc_tax - line_product_cost_exc_vat_metric,2)                                       as margin_value,
+    if(extract(hour from created_at_hour) < 10, '0', '') || extract(hour from created_at_hour) || ':00'     as hour,
+    null                                                                                                    as last_week_margin,
+    null                                                                                                    as last_week_qty_invoiced,
+    null                                                                                                    as last_week_revenue
+from metrics_today_and_last_week mt
 where date(created_at_hour) = current_date
+
+union all
+
+select
+    created_at_hour,
+    nego,
+    brand,
+    category_name,
+    department_type,
+    parent_sku,
+    product_type,
+    supplier_name,
+    null                                                                                                    as qty_invoiced_metric,
+    null                                                                                                    as total_revenue,
+    null                                                                                                    as line_product_cost_exc_vat_metric,
+    null                                                                                                    as total_revenue_exc_tax,
+    null                                                                                                    as margin_value,
+    if(extract(hour from created_at_hour) < 10, '0', '') || extract(hour from created_at_hour) || ':00'     as hour,
+    round(total_revenue_exc_tax - line_product_cost_exc_vat_metric,2)                                       as last_week_margin,
+    qty_invoiced_metric                                                                                     as last_week_qty_invoiced,
+    total_revenue_exc_tax                                                                                   as last_week_revenue
+from metrics_today_and_last_week mt
+where date(created_at_hour) = current_date - 7
+
