7f800653feff997e44736e86c31df62a8c94153a
Nicholas Lupu <njlupu@gmail.com>
2023-03-27 15:56:06 +0100

    ga stats to orderlines join

--- dbt/brandalley-dbt/models/google_analytics/ga_daily_stats.sql
+++ dbt/brandalley-dbt/models/google_analytics/ga_daily_stats.sql
@@ -28,6 +28,8 @@ select
     device.browser                                                                                            as device_browser,
     device.operatingSystem                                                                                    as device_os,
     product.productSKU                                                                                        as product_sku,
+    -- As we receive parent_sku as the sku - keep the offset to be able to join to OrderLines on a 1 to 1
+    i                                                                                                         as product_sku_offset,
     product.v2productName                                                                                     as product_name,
     product.productBrand                                                                                      as product_brand,
     product.productQuantity                                                                                   as product_quantity,
@@ -35,8 +37,11 @@ select
     coalesce(product.productRevenue/1000000,0)                                                                as product_revenue,
     fullVisitorId                                                                                             as visitor_id,
     visitId                                                                                                   as visit_id,
-    fullVisitorId || visitId                                                                                  as unique_visit_id
-from {{ source('76149814', 'ga_sessions_*') }},
+    fullVisitorId || visitId                                                                                  as unique_visit_id --,
+    {# hits.experiment.experimentId                                                                              as experiment_id, 
+    hits.experiment.experimentVariant                                                                         as experiment_variant #}
+from `bigquery-347014.76149814.ga_sessions_20230326`,
+{# from {{ source('76149814', 'ga_sessions_*') }}, #}
     unnest(hits) as hits
 left join unnest(hits.product) as product with offset as i
 where totals.visits = 1
