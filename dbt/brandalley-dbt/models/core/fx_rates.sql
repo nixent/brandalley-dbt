c5e1520a91ebf155503ac456a451ce9cf008b57d
Nicholas Lupu <njlupu@gmail.com>
2023-04-13 18:12:22 +0100

    add fx rates

---
+++ dbt/brandalley-dbt/models/core/fx_rates.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized='table'
+)}}
+
+select
+    Date           as date,
+    AUD            as gbp_to_aud,
+    CAD            as gbp_to_cad,
+    CHF            as gbp_to_chf,
+    EUR            as gbp_to_eur,
+    HKD            as gbp_to_hkd,
+    USD            as gbp_to_usd,
+    EUR_Budget     as gbp_to_eur_budget,
+    round(1/coalesce(EUR,EUR_Budget),5) as eur_to_gbp
+from {{ source('analytics', 'fx_rates_gsheet') }}
