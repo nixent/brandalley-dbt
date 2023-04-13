b1e8cabba884e56e07d2e794eb91ffd40a42a07e
Nicholas Lupu <njlupu@gmail.com>
2023-04-13 15:58:25 +0100

    start dedupe logic for fr/uk

--- dbt/brandalley-dbt/models/sources/streamkap_current/core/stg__admin_user.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/core/stg__admin_user.sql
@@ -1,6 +1,85 @@
 {{config(
     materialized='incremental',
-    unique_key= 'user_id'
+    unique_key= 'ba_site_user_id'
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='admin_user', id_field=config.get('unique_key'))}}
+{# with unioned as (
+    select
+    {{ dbt_utils.star(
+        ref('stg_uk__admin_user'),
+        quote_identifiers=false
+    ) }}
+    from {{ ref('stg_uk__admin_user') }}
+) #}
+
+select
+    'UK-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'UK'                                                            as ba_site,
+    user_id,
+    firstname,
+    lastname,
+    email,
+    username,
+    password,
+    created,
+    modified,
+    logdate,
+    lognum,
+    reload_acl_flag,
+    is_active,
+    extra,
+    rp_token,
+    rp_token_created_at,
+    failures_num,
+    first_failure,
+    lock_expires,
+    token_login_enabled,
+    login_token_secret,
+    last_token_used,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_uk__admin_user') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'UK' )
+{% endif %}
+
+union all
+
+select
+    'FR-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'FR'                                                            as ba_site,
+    user_id,
+    firstname,
+    lastname,
+    email,
+    username,
+    password,
+    cast(created as string),
+    cast(modified as string),
+    cast(logdate as string),
+    lognum,
+    reload_acl_flag,
+    is_active,
+    extra,
+    rp_token,
+    cast(rp_token_created_at as string),
+    failures_num,
+    cast(first_failure as string),
+    cast(lock_expires as string),
+    token_login_enabled,
+    login_token_secret,
+    last_token_used,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_fr__admin_user') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'FR' )
+{% endif %}
