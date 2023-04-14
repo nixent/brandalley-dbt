b5e614b10ba05ef989d5aa422efce971018f842a
Nicholas Lupu <njlupu@gmail.com>
2023-04-14 14:37:33 +0100

    invent referer uk fr

--- dbt/brandalley-dbt/models/sources/streamkap_current/stg__bacore_stock_return.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/stg__bacore_stock_return.sql
@@ -1,7 +1,34 @@
 {{config(
     materialized='incremental',
-    unique_key='id',
-	cluster_by='id',
+    unique_key='ba_site_id',
+	cluster_by='ba_site_id',
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='bacore_stock_return', id_field=config.get('unique_key'))}}
+-- to do when streamkap fix
+
+with unioned as (
+    select
+    {{ dbt_utils.star(
+        ref('stg_uk__bacore_stock_return'),
+        quote_identifiers=false
+    ) }}
+    from {{ ref('stg_uk__bacore_stock_return') }}
+)
+
+select
+    'UK-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'UK'                                                            as ba_site,
+from {{ ref('stg_uk__bacore_stock_return') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'UK' )
+{% endif %}
+
+union all
+
+select
+    'FR-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'FR'                                                            as ba_site,
+from {{ ref('stg_fr__bacore_stock_return') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'FR' )
+{% endif %}
