561fbad93e66b3433b809956fc38bdbdc9384e09
Nicholas Lupu <njlupu@gmail.com>
2023-04-14 15:38:15 +0100

    customer fr/uk merging

--- dbt/brandalley-dbt/models/sources/streamkap_current/customers/stg__customer_entity_extra.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/customers/stg__customer_entity_extra.sql
@@ -1,7 +1,51 @@
 {{config(
     materialized='incremental',
-    unique_key='line_id',
-	cluster_by='line_id',
+    unique_key='ba_site_line_id',
+	cluster_by='ba_site_line_id',
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='customer_entity_extra', id_field=config.get('unique_key'))}}
+select
+    'UK-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'UK'                                                            as ba_site,
+    line_id,
+    customer_id,
+    number_transactions,
+    ltv,
+    ltvr,
+    timestamp(first_purchase) as first_purchase,
+    timestamp(last_purchase) as last_purchase,
+    timestamp(first_visit_at) as first_visit_at,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_uk__customer_entity_extra') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'UK' )
+{% endif %}
+
+union all
+
+select
+    'FR-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'FR'                                                            as ba_site,
+    line_id,
+    customer_id,
+    number_transactions,
+    ltv,
+    ltvr,
+    first_purchase,
+    last_purchase,
+    first_visit_at,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_fr__customer_entity_extra') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'FR' )
+{% endif %}
--- dbt/brandalley-dbt/models/sources/streamkap_current/customers/stg__log_customer.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/customers/stg__log_customer.sql
@@ -1,7 +1,34 @@
 {{config(
     materialized='incremental',
-    unique_key='log_id',
+    unique_key='ba_site_log_id',
 	cluster_by='log_id',
 )}}
 
-{{streamkap_incremental_on_source_to_current(source_name='log_customer', id_field=config.get('unique_key'))}}
+-- TO DO with streamkap fix
+
+with unioned as (
+    select
+    {{ dbt_utils.star(
+        ref('stg_uk__log_customer'),
+        quote_identifiers=false
+    ) }}
+    from {{ ref('stg_uk__log_customer') }}
+)
+
+select
+    'UK-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'UK'                                                            as ba_site,
+from {{ ref('stg_uk__log_customer') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'UK' )
+{% endif %}
+
+union all
+
+select
+    'FR-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'FR'                                                            as ba_site,
+from {{ ref('stg_fr__log_customer') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'FR' )
+{% endif %}
