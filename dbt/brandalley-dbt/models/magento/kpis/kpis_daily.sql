4c1d60ff8a9c0291bcf8509c48a474dcbf80a030
Nicholas Lupu <njlupu@gmail.com>
2023-05-02 16:05:52 +0100

    fix kpis daily

--- dbt/brandalley-dbt/models/magento/kpis/kpis_daily.sql
+++ dbt/brandalley-dbt/models/magento/kpis/kpis_daily.sql
@@ -21,11 +21,11 @@ with order_stats as (
 customer_stats as (
     select
         if(crds.date is not null, date(crds.date), 
-            if(ce.achica_user is null OR ce.achica_user != 2, date(ce.achica_migration_date), date(datetime(ce.signed_up_at, "Europe/London")))
+            if(ce.achica_user is null or ce.achica_user != 2, date(ce.achica_migration_date), date(datetime(ce.signed_up_at, "Europe/London")))
         )                                                                               as customer_created_at_day,
-        ba_site,
-        count(if(achica_user is null or achica_user != 2, customer_id, null))           as total_new_members,
-        count(if(achica_user = 2, customer_id, null))                                   as total_new_achica_members
+        ce.ba_site,
+        count(if(ce.achica_user is null or ce.achica_user != 2, ce.customer_id, null))           as total_new_members,
+        count(if(ce.achica_user = 2, ce.customer_id, null))                                   as total_new_achica_members
     from {{ ref('customers_enriched') }} ce
     left join {{ ref('customers_record_data_source') }} crds 
         on ce.customer_id = crds.cst_id
