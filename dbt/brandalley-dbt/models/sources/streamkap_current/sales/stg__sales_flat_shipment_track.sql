afa551d7aa994b73a2c631da379da7f5e821cd51
guillaume.vachey@avora.com <guillaume.vachey@avora.com>
2023-05-26 13:02:02 +0000

    Adding date formats + sales_flat_shipment_track missing elements

---
+++ dbt/brandalley-dbt/models/sources/streamkap_current/sales/stg__sales_flat_shipment_track.sql
@@ -0,0 +1,57 @@
+{{config(
+    materialized='incremental',
+    unique_key='ba_site_entity_id',
+	cluster_by='entity_id',
+)}}
+
+select
+    'UK-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'UK'                                                            as ba_site,
+    entity_id,
+    parent_id,
+    weight,
+    qty,
+    order_id,
+    track_number,
+    description,
+    title,
+    carrier_code,
+    created_at,
+    updated_at,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_uk__sales_flat_shipment_track') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'UK' )
+{% endif %}
+
+union all
+
+select
+    'FR-' || {{ config.get('unique_key')|replace('ba_site_', '') }} as {{ config.get('unique_key') }},
+    'FR'                                                            as ba_site,
+    entity_id,
+    parent_id,
+    weight,
+    qty,
+    order_id,
+    track_number,
+    description,
+    title,
+    carrier_code,
+    created_at,
+    updated_at,
+    _streamkap_source_ts_ms,
+    _streamkap_ts_ms,
+    _streamkap_offset,
+    _streamkap_loaded_at_ts,
+    __deleted,
+    bq_last_processed_at
+from {{ ref('stg_fr__sales_flat_shipment_track') }}
+{% if is_incremental() %}
+    where bq_last_processed_at > (select max(bq_last_processed_at) from {{this}} where ba_site = 'FR' )
+{% endif %}
