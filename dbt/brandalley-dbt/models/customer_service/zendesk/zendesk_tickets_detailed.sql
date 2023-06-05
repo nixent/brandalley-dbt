284143a72ddf59e967221358bf1908716c6b7a68
guillaume.vachey@avora.com <guillaume.vachey@avora.com>
2023-06-05 16:06:52 +0000

    Updating the logic for carrier_codes to take in account multiple carriers and adding a fix for shipments join to orderlines which was incorrect

--- dbt/brandalley-dbt/models/customer_service/zendesk/zendesk_tickets_detailed.sql
+++ dbt/brandalley-dbt/models/customer_service/zendesk/zendesk_tickets_detailed.sql
@@ -4,42 +4,44 @@
 ) }}
 
 with tickets as (
-    select  id,
-            created_at,
-            description,
-            due_at,
-            is_public,
-            merged_ticket_ids,
-            status,
-            subject,
-            updated_at,
-            type,
-            url,
-            via_channel,
-            via_source_rel,
-            if(via_channel='voice', 1, 0) as phone_ticket,
-            if(via_channel='native_messaging', 1, 0) as chat_ticket,
-            if(via_channel in ('web','email'), 1, 0) as web_email_ticket,
-            if(via_channel='web', 1, 0) as web_ticket,
-            if(via_channel='email', 1, 0) as email_ticket,
-            custom_carrier,
-            custom_client_contact_reason_no_order,
-            custom_client_contact_reason_order,
-            custom_contact_reason,
-            custom_coupon_code,
-            custom_needs_senior_help,
-            custom_order_id,
-            custom_order_number,
-            custom_order_related,
-            custom_product_issue_type,
-            custom_shipping_issue_type,
-            custom_solution,
-            custom_status_id,
-            custom_tracking_,
-            consignment_qty, 
-            warehouse_qty, 
-            selffulfill_qty,
-            order_id
+    select  ticket.id,
+            ticket.created_at,
+            ticket.description,
+            ticket.due_at,
+            ticket.is_public,
+            ticket.merged_ticket_ids,
+            ticket.status,
+            ticket.subject,
+            ticket.updated_at,
+            ticket.type,
+            ticket.url,
+            ticket.via_channel,
+            ticket.via_source_rel,
+            if(ticket.via_channel='voice', 1, 0) as phone_ticket,
+            if(ticket.via_channel='native_messaging', 1, 0) as chat_ticket,
+            if(ticket.via_channel in ('web','email'), 1, 0) as web_email_ticket,
+            if(ticket.via_channel='web', 1, 0) as web_ticket,
+            if(ticket.via_channel='email', 1, 0) as email_ticket,
+            ticket.custom_carrier,
+            ticket.custom_client_contact_reason_no_order,
+            ticket.custom_client_contact_reason_order,
+            ticket.custom_contact_reason,
+            ticket.custom_coupon_code,
+            ticket.custom_needs_senior_help,
+            ticket.custom_order_id,
+            ticket.custom_order_number,
+            ticket.custom_order_related,
+            ticket.custom_product_issue_type,
+            ticket.custom_shipping_issue_type,
+            ticket.custom_solution,
+            ticket.custom_status_id,
+            ticket.custom_tracking_,
+            orderlines.consignment_qty, 
+            orderlines.warehouse_qty, 
+            orderlines.selffulfill_qty,
+            orderlines.order_id,
+            -- we removing duplicates from the liste of carriers in case an order had multiple shipments with the same carrier
+            (select string_agg(distinct trim(value)) from UNNEST(SPLIT(carrier_codes_dupes, ',')) as value) as carrier_codes
     from {{ source(
         'zendesk',
         'ticket'
@@ -50,10 +52,12 @@ with tickets as (
     where ba_site='UK'
     group by order_number, order_id) orderlines
     on IFNULL(ticket.custom_order_id, ticket.custom_order_number) = orderlines.order_number
-	where 1=1
+    left outer join (select distinct string_agg(carrier_code) OVER(PARTITION BY order_id) carrier_codes_dupes, order_id from {{ ref('stg__sales_flat_shipment_track') }}) track
+    on orderlines.order_id = track.order_id
+	where 1=1 and track.order_id=8476877
 /*	{% if is_incremental() %}
 		and updated_at >= '{{min_ts}}'
 	{% endif %}*/
 )
 
-select * from tickets order by id desc
+select * from tickets
