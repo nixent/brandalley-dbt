f21b20c2a75ba76bb5ae96feaf5d0268dcf9554e
Nicholas Lupu <njlupu@gmail.com>
2023-06-23 09:15:43 +0100

    product reference fix

--- dbt/brandalley-dbt/models/sources/streamkap_current_fr/catalog/stg_fr__catalog_product_reference.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current_fr/catalog/stg_fr__catalog_product_reference.sql
@@ -1,8 +1,7 @@
 {{config(
     materialized='incremental',
     unique_key='reference_id',
-	cluster_by='reference_id',
-    enabled=false
+	cluster_by='reference_id'
 )}}
 
 {{streamkap_incremental_on_source_to_current(source_name='catalog_product_reference', id_field=config.get('unique_key'))}}
