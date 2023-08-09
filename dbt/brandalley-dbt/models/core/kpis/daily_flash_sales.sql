3ef343ccb4dc2584944e76d34a5401a15eabd32a
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-08-09 14:38:47 +0000

    flash sale end

--- dbt/brandalley-dbt/models/core/kpis/daily_flash_sales.sql
+++ dbt/brandalley-dbt/models/core/kpis/daily_flash_sales.sql
@@ -38,13 +38,18 @@ with
             d.date_day,
             week_num,
             ps.ba_site,
-            ifnull(count(distinct gs.category_name), 0) as sales_starting_at_date
+            ifnull(count(distinct gs_start.category_name), 0) as sales_starting_at_date,
+            ifnull(count(distinct gs_end.category_name), 0) as sales_ending_at_date,
         from daily_dates d
         cross join (select distinct ba_site from {{ ref("products_sales") }}) ps
         left join
-            grouped_sales gs
-            on d.date_day = cast(gs.sale_start_at as date)
-            and ps.ba_site = gs.ba_site
+            grouped_sales gs_start
+            on d.date_day = cast(gs_start.sale_start_at as date)
+            and ps.ba_site = gs_start.ba_site
+        left join
+            grouped_sales gs_end
+            on d.date_day = cast(gs_end.sale_end_at as date)
+            and ps.ba_site = gs_end.ba_site
         group by 1, 2, 3
 
     )
@@ -55,7 +60,9 @@ select
     fs.ba_site,
     fs.live_sales_count,
     ifnull(fss.sales_starting_at_date, 0) as sales_starting_at_date,
-    sum(ifnull(fss1.sales_starting_at_date, 0)) as same_week_future_sales_starting
+    ifnull(fss.sales_ending_at_date, 0) as sales_ending_at_date,
+    sum(ifnull(fss1.sales_starting_at_date, 0)) as same_week_future_sales_starting,
+    sum(ifnull(fss1.sales_ending_at_date, 0)) as same_week_future_sales_ending
 from flash_sales fs
 left join
     flash_sales_start fss on fs.date_day = fss.date_day and fs.ba_site = fss.ba_site
@@ -64,4 +71,5 @@ left join
     on fss.week_num = fss1.week_num
     and fss.ba_site = fss1.ba_site
     and fss.date_day < fss1.date_day
-group by 1, 2, 3, 4, 5
+group by 1, 2, 3, 4, 5, 6
+order by fs.date_day desc, fs.ba_site
