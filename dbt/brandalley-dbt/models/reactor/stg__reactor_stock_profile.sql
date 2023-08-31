eb92b469c3f3596f3bd0143972d523d66ccc6f6c
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-08-31 14:06:30 +0000

    reactor stock profile

---
+++ dbt/brandalley-dbt/models/reactor/stg__reactor_stock_profile.sql
@@ -0,0 +1,111 @@
+{{ config(materialized="table") }}
+
+with raw_stock_profile as (
+select
+    cast(current_date as date) as logged_date,
+    bsi.stockid as reactor_sku_id,
+    sl.legacy_id as sku,
+    bsi.quantity as quantity,
+    bsi.boxid as box_id,
+    br.id as rack_id,
+    br.row as rack_row,
+    br.rack as rack,
+    br.racknumber as rack_number,
+    aba.aisleid as aisle_id,
+    aba.aislenumber as aisle_number,
+    abz.zoneid as zone_id,
+    abz.name as zone_name,
+    abz.abbr as zone_abbr,
+    abz.color as zone_colour,
+    abz.pickable as is_zone_pickable,
+    'available' as stock_type
+from {{ ref("stg__boxstockindex") }} as bsi
+left join {{ ref("stg__box") }} as b on bsi.boxid = b.id
+left join {{ ref("stg__boxracking") }} as br on br.id = b.boxrackingid
+left join {{ ref("stg__adboxaisle") }} as aba on aba.aisleid = br.aisleid
+left join {{ ref("stg__adboxzone") }} as abz on abz.zoneid = aba.zoneid
+left join {{ ref("stg__stocklist") }} sl on bsi.stockid = sl.id
+
+union all
+
+select
+    cast(current_date as date) as logged_date,
+    o.stockid as reactor_sku_id,
+    sl.legacy_id as sku,
+    o.quantity,
+    obi.boxid as box_id,
+    br.id as rack_id,
+    br.row as rack_row,
+    br.rack as rack,
+    br.racknumber as rack_number,
+    aba.aisleid as aisle_id,
+    aba.aislenumber as aisle_number,
+    abz.zoneid as zone_id,
+    abz.name as zone_name,
+    abz.abbr as zone_abbr,
+    abz.color as zone_colour,
+    abz.pickable as is_zone_pickable,
+    'allocated' as stock_type
+from {{ ref("stg__orderboxindex") }} as obi
+inner join {{ ref("stg__orders") }} as o on obi.orderid = o.orderid
+left join {{ ref("stg__box") }} as b on obi.boxid = b.id
+left join {{ ref("stg__boxracking") }} as br on br.id = b.boxrackingid
+left join {{ ref("stg__adboxaisle") }} as aba on aba.aisleid = br.aisleid
+left join {{ ref("stg__adboxzone") }} as abz on abz.zoneid = aba.zoneid
+left join {{ ref("stg__stocklist") }} sl on o.stockid = sl.id
+where obi.__deleted = false
+
+union all
+
+select
+    cast(current_date as date) as logged_date,
+    o.stockid as reactor_sku_id,
+    sl.legacy_id as sku,
+    sum(o.quantity) as quantity,
+    null as box_id,
+    null as rack_id,
+    null as rack_row,
+    null as rack,
+    null as rack_number,
+    null as aisle_id,
+    null as aisle_number,
+    null as zone_id,
+    null as zone_name,
+    null as zone_abbr,
+    null as zone_colour,
+    0 as is_zone_pickable,
+    'pending dispatch' as stock_type
+from {{ ref("stg__orders") }} as o
+inner join
+    {{ ref("stg__customers") }} as c
+    on c.customerid = o.customerid
+    and c.deleted is null
+left join
+    (select * from {{ ref("stg__orderboxindex") }} where __deleted = false) obi
+    on obi.orderid = o.orderid
+left join {{ ref("stg__stocklist") }} sl on o.stockid = sl.id
+where
+    o.completed_timestamp is not null
+    and datetime_add('1970-01-01', interval o.leftwarehouse_timestamp second)
+    < '2000-01-01'
+    and obi.id is null
+group by o.stockid, sl.legacy_id
+)
+select
+	rsp.logged_date,
+	rsp.sku,
+	rsp.reactor_sku_id as reactor_sku_id,
+	sum(rsp.quantity) as on_hand,
+	sum(case when rsp.stock_type in ('allocated', 'pending dispatch')
+		then rsp.quantity
+	    else 0 end) as allocated,
+	sum(case when rsp.stock_type = 'available'
+		then rsp.quantity
+	    else 0 end) as available,
+	0 as goods_in_quarantine,
+	0 as returns_quarantine,
+	0 as faulty_quarantine,
+	0 as other_quarantine
+from raw_stock_profile as rsp
+--where rsp.logged_date >= convert(date, @date)
+group by 1,2,3
