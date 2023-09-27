e12c0174f35536f4879004fb3f3f8e6c79d36e1c
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-09-27 12:34:58 +0000

    stock age daily

---
+++ dbt/brandalley-dbt/models/magento/catalog/stock_age_daily.sql
@@ -0,0 +1,14 @@
+
+{{ config(
+    full_refresh=false,
+    on_schema_change='sync_all_columns',
+    materialized='incremental'
+) }}
+
+select 
+    current_timestamp as processed_at,
+    * 
+from {{ ref('stock_age') }}
+{% if is_incremental() %}
+where current_date > (select max(logged_date) from {{this}})
+{% endif %}
