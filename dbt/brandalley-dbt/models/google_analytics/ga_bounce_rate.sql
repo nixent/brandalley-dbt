63eb8144ac3958160d47eb542326fd3dd646b752
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-10-19 11:12:04 +0000

    bounce rate rework, and renaming metrics

--- dbt/brandalley-dbt/models/google_analytics/ga_bounce_rate.sql
+++ dbt/brandalley-dbt/models/google_analytics/ga_bounce_rate.sql
@@ -1,4 +1,7 @@
-{{ config(materialized="table", tags=["job_daily"]) }}
+{{ config(
+    materialized='incremental',
+    tags=["job_daily"]
+)}}
 
 with
     number_of_pages_per_sessions as (
@@ -7,6 +10,10 @@ with
             a.unique_visit_id,
             count(distinct a.page_path) as number_of_pages
         from {{ ref("ga_daily_stats") }} a
+        where cast(a.visit_start_at as date) < current_date --so we only load full previous day at a time
+        {% if is_incremental() %}
+        and cast(a.visit_start_at as date) >= (select max(logged_date) from {{this}})
+        {% endif %}
         group by 1, 2
     ),
     sessions_with_one_page as (
@@ -17,32 +24,32 @@ with
     ),
     bounce_rate as (
         select
-            cast(a.visit_start_at as date) as date,
+            a.date,
             count(distinct a.unique_visit_id) as sessions_total,
-            count(distinct concat(a.unique_visit_id, a.page_path)) as page_views_total,
+            sum(a.number_of_pages) as page_views_total,
             b.number_of_sessions_with_one_page,
-            safe_divide(number_of_sessions_with_one_page, count(distinct a.unique_visit_id))*100 as bounce_rate
-        from {{ ref("ga_daily_stats") }} a
-        left join sessions_with_one_page b on cast(a.visit_start_at as date) = b.date
+            safe_divide(b.number_of_sessions_with_one_page, count(distinct a.unique_visit_id))*100 as bounce_rate
+        from number_of_pages_per_sessions a
+        left join sessions_with_one_page b on a.date = b.date
         group by 1, 4
     ),
-    average_last_12_months as (
+    average_last_12_months as ( --these are average of last 12 months to current date
         select
             avg(sessions_total) as last_12_months_avg_sessions_total,
             avg(page_views_total) as last_12_months_avg_page_views_total,
             avg(bounce_rate) as last_12_months_avg_bounce_rate
         from bounce_rate a
         inner join
-            {{ ref("dates") }} b on a.date = b.date_day and b.last_12_months_flag = 1
+            {{ ref("dates") }} b on a.date = b.date_day and b.last_12_months_flag = True
     )
 select
     date as logged_date,
     sessions_total,
     page_views_total,
+    number_of_sessions_with_one_page,
     bounce_rate,
     round(last_12_months_avg_sessions_total, 2) as last_12_months_avg_sessions_total,
     round(last_12_months_avg_page_views_total, 2) as last_12_months_avg_page_views_total,
     round(last_12_months_avg_bounce_rate, 2) as last_12_months_avg_bounce_rate
 from bounce_rate a
 cross join average_last_12_months b
-order by date desc
