0b06b8b37e5e130d04ac617eb3535c954102b3fd
Nicholas Lupu <njlupu@gmail.com>
2023-10-19 13:09:33 +0100

    stock file daily updates

--- dbt/brandalley-dbt/models/magento/catalog/stock_file_daily.sql
+++ dbt/brandalley-dbt/models/magento/catalog/stock_file_daily.sql
@@ -3,6 +3,12 @@
     full_refresh=false,
     on_schema_change='sync_all_columns',
     materialized='incremental',
+    partition_by = {
+      "field": "stock_file_date",
+      "data_type": "date",
+      "granularity": "day"
+    },
+    pre_hook='delete from {{this}} where stock_file_date = current_date'
     tags=["job_daily"]
 ) }}
 
