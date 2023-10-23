32ac62ca31d7951cb34de628ec3429d2f6dc2d0c
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-10-23 13:32:09 +0000

    incremental logic update

--- dbt/brandalley-dbt/models/google_analytics/ga_bounce_rate.sql
+++ dbt/brandalley-dbt/models/google_analytics/ga_bounce_rate.sql
@@ -7,19 +7,19 @@
 with
     number_of_pages_per_sessions as (
         select
-            cast(a.visit_start_at as date) as date,
+            cast(a.visit_start_at as date) as logged_date,
             a.unique_visit_id,
             count(distinct a.page_path) as number_of_pages
         from {{ ref("ga_daily_stats") }} a
         where cast(a.visit_start_at as date) < current_date --so we only load full previous day at a time
         {% if is_incremental() %}
-        and cast(a.visit_start_at as date) > (select max(logged_date) from {{this}})
+        and cast(a.visit_start_at as date) >= (select max(logged_date) from {{this}})
         {% endif %}
         group by 1, 2
     ),
     bounce_rate as (
         select
-            a.date,
+            a.logged_date,
             count(distinct a.unique_visit_id) as sessions_total,
             sum(a.number_of_pages) as page_views_total,
             count(distinct (if (a.number_of_pages = 1, a.unique_visit_id, null))) as number_of_sessions_with_one_page,
@@ -33,10 +33,10 @@ with
             avg(page_views_total) as last_12_months_avg_page_views_total,
             avg(bounce_rate) as last_12_months_avg_bounce_rate
         from bounce_rate a
-        where a.date > date_sub(current_date, interval 1 year)
+        where a.logged_date > date_sub(current_date, interval 1 year)
     )
 select
-    date as logged_date,
+    logged_date,
     sessions_total,
     page_views_total,
     number_of_sessions_with_one_page,
@@ -46,4 +46,4 @@ select
     round(last_12_months_avg_bounce_rate, 2) as last_12_months_avg_bounce_rate
 from bounce_rate a
 cross join average_last_12_months b
-order by date desc
+order by logged_date desc
