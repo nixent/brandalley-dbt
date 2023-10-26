13306e779d5194357e47dc78d1dcfa7aef1a1fea
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-10-26 13:39:11 +0000

    adding schema to config

--- dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
+++ dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
@@ -1,5 +1,6 @@
 {{ config(
-    materialized='table'
+    materialized='table',
+    schema='marketing'
 )}}
 
 with cj_affiliates as (
@@ -17,19 +18,14 @@ select date,
        cast(sale_amount_adv_currency as numeric) as sales_amount_before_vouchers,
        case when cast(sale_amount_adv_currency as numeric) > 0 then cast(order_discount_adv_currency as numeric) else cast(order_discount_adv_currency as numeric)*-1 end as order_discount,
        cast(adv_commission_amount_adv_currency as numeric) as adv_commission_amount, --need to make sure this is this all gbp
-       cast(pub_commission_amount_pub_currency as numeric) as pub_commission_amount,
        cast(cj_fee_adv_currency as numeric) as cj_fee_amount,
        case when cast(sale_amount_adv_currency as numeric) > 0 then 'Order' else 'Return' end as transaction_type,
        coupon,
        new_to_file,
        is_cross_device        
 from {{ source('cj_affiliates', 'cj_affiliates_commission') }}
-),
-final as (
+)
 select *,
        sales_amount_before_vouchers - order_discount as sales_amount_after_vouchers,
-       adv_commission_amount + pub_commission_amount + cj_fee_amount as total_fee
-from cj_affiliates 
-where date between '2023-06-01' and '2023-06-30'
-)
-select case when total_fee=0 then 'matches' else ' no match' end as matches_sheet, count(*) from final group by 1
+       abs(adv_commission_amount + cj_fee_amount) as pub_commission_amount --adv commission is negative whilst cj fee is positive. Always totals to 0 in sheet
+from cj_affiliates
