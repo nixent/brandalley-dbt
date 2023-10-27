a0c7493b4d114b33828078a629f828c728032f5a
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-10-27 14:30:42 +0000

    fixes

--- dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
+++ dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized='table',
-    schema='marketing'
+    schema='marketing',
+    tags=["job_daily"]
 )}}
 
 with cj_affiliates as (
@@ -15,7 +16,7 @@ select date,
        action_tracker_id as action_id,
        action_tracker_name as action_name,
        action_status as status,
-       cast(sale_amount_adv_currency as numeric) as sales_amount_before_vouchers,
+       cast(sale_amount_adv_currency as numeric) as gmv_before_vouchers,
        case when cast(sale_amount_adv_currency as numeric) > 0 then cast(order_discount_adv_currency as numeric) else cast(order_discount_adv_currency as numeric)*-1 end as order_discount, --this is because there are return lines and the discount amount is positive for both
        cast(adv_commission_amount_adv_currency as numeric) as adv_commission_amount,
        cast(cj_fee_adv_currency as numeric) as cj_fee_amount,
@@ -26,6 +27,6 @@ select date,
 from {{ source('cj_affiliates', 'cj_affiliates_commission') }}
 )
 select *,
-       sales_amount_before_vouchers - order_discount as sales_amount_after_vouchers,
+       gmv_before_vouchers - order_discount as gmv_after_vouchers,
        abs(adv_commission_amount + cj_fee_amount) as pub_commission_amount --adv commission is negative whilst cj fee is positive. Always totals to 0 in sheet
 from cj_affiliates
