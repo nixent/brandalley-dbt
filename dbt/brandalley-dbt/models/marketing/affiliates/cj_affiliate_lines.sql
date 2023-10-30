73fdb449133df3fbdea4ae6ff4b091d4f712f599
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-10-30 16:35:27 +0000

    affiliate and bounce rate changes

--- dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
+++ dbt/brandalley-dbt/models/marketing/affiliates/cj_affiliate_lines.sql
@@ -16,10 +16,10 @@ select date,
        action_tracker_id as action_id,
        action_tracker_name as action_name,
        action_status as status,
-       cast(sale_amount_adv_currency as numeric) as gmv_before_vouchers,
+       cast(sale_amount_adv_currency as numeric) as sales_amount_after_vouchers,
        case when cast(sale_amount_adv_currency as numeric) > 0 then cast(order_discount_adv_currency as numeric) else cast(order_discount_adv_currency as numeric)*-1 end as order_discount, --this is because there are return lines and the discount amount is positive for both
        cast(adv_commission_amount_adv_currency as numeric) as adv_commission_amount,
-       cast(cj_fee_adv_currency as numeric) as cj_fee_amount,
+       cast(cj_fee_adv_currency as numeric)*-1 as cj_fee_amount, --formatting so its in line with other fees/commission
        case when cast(sale_amount_adv_currency as numeric) > 0 then 'Order' else 'Return' end as transaction_type,
        coupon,
        new_to_file,
@@ -27,6 +27,5 @@ select date,
 from {{ source('cj_affiliates', 'cj_affiliates_commission') }}
 )
 select *,
-       gmv_before_vouchers - order_discount as gmv_after_vouchers,
-       abs(adv_commission_amount + cj_fee_amount) as pub_commission_amount --adv commission is negative whilst cj fee is positive. Always totals to 0 in sheet
+       adv_commission_amount - cj_fee_amount as pub_commission_amount
 from cj_affiliates
