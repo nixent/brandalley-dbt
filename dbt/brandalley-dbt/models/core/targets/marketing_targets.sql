2557eff33fbcbe280afd0d25fe54a1cd1d86430a
Nicholas Lupu <njlupu@gmail.com>
2023-10-31 12:45:21 +0000

    fix broken models

--- dbt/brandalley-dbt/models/core/targets/marketing_targets.sql
+++ dbt/brandalley-dbt/models/core/targets/marketing_targets.sql
@@ -5,21 +5,21 @@
 
 
 select 
-    Date                   as target_date,
+    target_date,
     'UK'                   as ba_site,
-    UK_Orders_RC_Forecast  as returning_customers_order_forecast,
-    UK_NC_Forecast         as new_customers_order_forecast,
-    UK_Members_Forecast    as new_members_forecast
+    uk_returning_customers_order_forecast,
+    uk_new_customers_order_forecast,
+    uk_new_members_forecast
 from {{ source('analytics', 'marketing_targets_gsheet') }}
-where Date is not null
+where target_date is not null
 
 union all
 
 select 
-    Date                   as target_date,
+    target_date,
     'FR'                   as ba_site,
-    FR_Orders_RC_Forecast  as returning_customers_order_forecast,
-    FR_NC_Forecast         as new_customers_order_forecast,
-    FR_Members_Forecast    as new_members_forecast
+    fr_returning_customers_order_forecast,
+    fr_new_customers_order_forecast,
+    fr_new_members_forecast
 from {{ source('analytics', 'marketing_targets_gsheet') }}
-where Date is not null
+where target_date is not null
