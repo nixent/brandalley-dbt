54325bce5875078520212f9be20c6ad433d56d96
Nicholas Lupu <njlupu@gmail.com>
2023-10-31 12:51:37 +0000

    column naming

--- dbt/brandalley-dbt/models/core/targets/marketing_targets.sql
+++ dbt/brandalley-dbt/models/core/targets/marketing_targets.sql
@@ -6,10 +6,10 @@
 
 select 
     target_date,
-    'UK'                   as ba_site,
-    uk_returning_customers_order_forecast,
-    uk_new_customers_order_forecast,
-    uk_new_members_forecast
+    'UK'                                    as ba_site,
+    uk_returning_customers_order_forecast   as returning_customers_order_forecast,
+    uk_new_customers_order_forecast         as new_customers_order_forecast,
+    uk_new_members_forecast                 as new_members_forecast
 from {{ source('analytics', 'marketing_targets_gsheet') }}
 where target_date is not null
 
@@ -17,9 +17,9 @@ union all
 
 select 
     target_date,
-    'FR'                   as ba_site,
-    fr_returning_customers_order_forecast,
-    fr_new_customers_order_forecast,
-    fr_new_members_forecast
+    'FR'                                    as ba_site,
+    fr_returning_customers_order_forecast   as returning_customers_order_forecast,
+    fr_new_customers_order_forecast         as new_customers_order_forecast,
+    fr_new_members_forecast                 as new_members_forecast
 from {{ source('analytics', 'marketing_targets_gsheet') }}
 where target_date is not null
