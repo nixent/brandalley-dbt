6227a45c4aa91f2b6a05968f6c8daaa4bf7daf61
Nicholas Lupu <njlupu@gmail.com>
2023-11-08 10:45:39 +0000

    add shipments to wh

--- dbt/brandalley-dbt/models/magento/sales/shipping/shipments.sql
+++ dbt/brandalley-dbt/models/magento/sales/shipping/shipments.sql
@@ -1,20 +1,20 @@
-SELECT
-    entity_id,
-    ba_site,
-    store_id,
-    total_weight,
-    total_qty,
-    email_sent,
-    order_id,
-    customer_id,
-    shipping_address_id,
-    billing_address_id,
-    shipment_status,
-    increment_id,
-    cast(created_at as timestamp) as created_at,
-    updated_at,
-    packages
-FROM
-    {{ ref(
-        'stg__sales_flat_shipment'
-    ) }}
+select
+    sfs.entity_id,
+    sfs.ba_site,
+    sfs.store_id,
+    sfs.total_weight,
+    sfs.total_qty,
+    sfs.email_sent,
+    sfs.order_id,
+    sfs.customer_id,
+    sfs.shipping_address_id,
+    sfs.billing_address_id,
+    sfs.shipment_status,
+    sfs.increment_id,
+    cast(sfs.created_at as timestamp) as created_at,
+    sfs.updated_at,
+    sfs.packages,
+    if(pdos.warehouse_id is not null, true, false) as is_wh_shipment
+from {{ ref('stg__sales_flat_shipment') }} sfs
+left join {{ ref('stg_uk__prism_dispatch_order_shipments') }} pdos
+on sfs.entity_id = pdos.magento_shipment_id and sfs.ba_site = 'UK'
