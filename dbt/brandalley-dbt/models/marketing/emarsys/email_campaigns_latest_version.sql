0a84b712c731b5516997c5e201c742acf6f64469
Nicholas Lupu <njlupu@gmail.com>
2023-11-09 11:31:48 +0000

    fix config

--- dbt/brandalley-dbt/models/marketing/emarsys/email_campaigns_latest_version.sql
+++ dbt/brandalley-dbt/models/marketing/emarsys/email_campaigns_latest_version.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table'), tags=["emarsys_eu"] }}
+{{ config(materialized='table', tags=["emarsys_eu"]) }}
 
 select * from {{ ref('email_campaigns') }}
 qualify row_number() over (partition by campaign_id order by event_time) = 1
