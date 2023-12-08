b7bba5577a2a4a17b5712f68c171ef0c31f038b8
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2023-12-08 12:52:28 +0000

    sales forecast

---
+++ dbt/brandalley-dbt/models/core/forecasts/sales_forecast.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized='table'
+) }}
+
+
+select 
+    month_start_date,
+    month_name,
+    department,
+    sales_forecast
+from {{ source('core', 'stock_sales_forecast_gsheet') }}
+where target_date is not null
