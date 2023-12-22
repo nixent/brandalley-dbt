3cc6a7b74a6b71213f8c7dd45359c495fb08a7c8
Nicholas Lupu <njlupu@gmail.com>
2023-12-22 14:01:25 +0000

    reactor stock tables

---
+++ dbt/brandalley-dbt/models/sources/streamkap_current_reactor/stg__stock_audit_allocated.sql
@@ -0,0 +1,7 @@
+{{config(
+    materialized='incremental',
+    unique_key='stock_id',
+	cluster_by='stock_id',
+)}}
+
+{{streamkap_incremental_on_source_to_current(source_name='stock_audit_allocated', id_field=config.get('unique_key'))}}
