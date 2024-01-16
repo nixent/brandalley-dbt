103e7e2f8ea5833fbcdec57bf070fd8f68b010cf
daniel.plimley@brandalley.co.uk <daniel.plimley@brandalley.co.uk>
2024-01-16 13:09:50 +0000

    changing newly received qty logic

--- dbt/brandalley-dbt/models/reactor/reactor_purchase_orders_logs.sql
+++ dbt/brandalley-dbt/models/reactor/reactor_purchase_orders_logs.sql
@@ -1,33 +1,13 @@
 {{ config(materialized="table", tags=["job_daily"]) }}
 
---with
---    arrived_date as (
---        select
---            purchaseid,
---            stockid,
---            date(timestamp_seconds(timestamp)) as arrival_date,
---            sum(newly_received_quantity) over (
---                partition by purchaseid, stockid, date(timestamp_seconds(timestamp))
---            ) as sum_of_arrived
---        from {{ ref("stg__purchasestocklog") }}
---    ),
---    grouped as (
---        select purchaseid, stockid, arrival_date, sum_of_arrived
---        from arrived_date
---        group by 1, 2, 3, 4
---        qualify
---            row_number() over (
---                partition by purchaseid, stockid order by sum_of_arrived desc
---            )
---            = 1
---    )
+
+with po_info as (
 select
-    cast(a.purchaseid as string) as po_number,
-    cast(a.stockid as string) as reactor_sku,
+    a.purchaseid,
+    a.stockid,
     c.legacy_id as magento_sku,
     d.productname as productname,
     date(timestamp_seconds(a.ordertime)) as created_date,
-    date(timestamp_seconds(a.ordertime)) as arrived_date,
     case
         when a.orderquantity = a.receivedquantity
         then 'fulfilled'
@@ -38,12 +18,6 @@ select
         when a.receivedquantity = 0
         then 'on order'
     end as status,
-    a.orderquantity as qty_ordered,
-    a.receivedquantity as qty_received,
-    a.orderquantity-a.receivedquantity as qty_outstanding,
-    a.orderquantity*c.item_cost as value_ordered,
-    a.receivedquantity*c.item_cost as value_received,
-    (a.orderquantity-a.receivedquantity)*c.item_cost as value_outstanding,
     b.name as supplier,
     case when a.potype = 'bk2bk' then 'X Dock' else 'Stock' end as stock_type,
     c.item_cost as unit_cost,
@@ -66,4 +40,25 @@ from {{ ref("stg__purchasestock") }} a
 left join {{ ref("stg__stockist") }} b on a.supplierid = b.stockistid
 left join {{ ref("stg__stocklist") }} c on a.stockid = c.id
 left join {{ ref("stg__product") }} d on c.productid = d.productid
-
+qualify row_number() over (
+                partition by purchaseid, stockid order by 3,4,5,6,7,8,9,10
+            )
+            = 1
+)
+select
+    cast(a.purchaseid as string) as po_number,
+    cast(a.stockid as string) as reactor_sku,
+    date(timestamp_seconds(a.timestamp)) as arrival_date,
+    sum(newly_received_quantity) as newly_received_qty,
+    sum(newly_received_quantity*unit_cost) as newly_received_value,
+    magento_sku,
+    productname,
+    created_date,
+    status,
+    supplier,
+    stock_type, 
+    unit_cost,
+    whistl_stock_type
+from {{ ref("stg__purchasestocklog") }} a
+left join po_info b on a.purchaseid=b.purchaseid and a.stockid=b.stockid
+group by 1,2,3,6,7,8,9,10,11,12,13
